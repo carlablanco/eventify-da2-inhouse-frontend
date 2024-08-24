73f86d81a3af81e32d923ef51c17239edb1d4227
Pablo Cappetta <75391203+pablocappetta@users.noreply.github.com>
2024-08-24 13:15:35 -0300

    Develop (#1)
    * Initial commit with core configurations
    
    * Initial commit with core configurations (2)

---
+++ app/(site)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <body>
+      <main>
+        <div className={"flex items-center justify-center my-24"}>
+          <h1 className={"text-4xl"}>Welcome to Eventify</h1>
+        </div>
+      </main>
+    </body>
+  );
+}
