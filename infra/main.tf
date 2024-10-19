5d9faf9d4e7ffae0ed0abba81bc02de99b98c416
Carla <88563106+carlablanco@users.noreply.github.com>
2024-10-19 20:30:46 -0300

    que pasa si pusheo el tfstate solo

--- infra/main.tf
+++ infra/main.tf
@@ -1,21 +1,14 @@
 provider "aws" {
-  region = "us-east-1"
-}
-
-data "aws_ecr_repository" "existing" {
-  count = length(data.aws_ecr_repositories.all.repositories) > 0 ? 1 : 0
-  repository_name = "frontend-repo"
+  region     = "us-east-1"
 }
 
 resource "aws_ecr_repository" "ecr_repo" {
-  count = data.aws_ecr_repository.existing.count == 0 ? 1 : 0
   name = "frontend-repo"
-
   image_scanning_configuration {
     scan_on_push = true
   }
 }
 
 output "repository_url" {
-  value = aws_ecr_repository.ecr_repo[0].repository_url
+  value = aws_ecr_repository.ecr_repo.repository_url
 }
