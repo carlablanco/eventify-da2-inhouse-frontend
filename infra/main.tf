5d7fb7dade1a1bf7529a98308eb3ebe554e81e37
Carla <88563106+carlablanco@users.noreply.github.com>
2024-10-19 00:25:29 -0300

    deploy + main.tf

---
+++ infra/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_ecr_repository" "ecr_repo" {
+  name = "repo-frontend"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+output "repository_url" {
+  value = aws_ecr_repository.ecr_repo.repository_url
+}
