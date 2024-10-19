7e1e0663e5d1102be38c92a644e16f193f3a84e4
Carla <88563106+carlablanco@users.noreply.github.com>
2024-10-19 19:37:33 -0300

    a

--- infra/main.tf
+++ infra/main.tf
@@ -1,22 +1,5 @@
 provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
-}
-
-variable "aws_access_key_id" {
-  type        = string
-  description = "AWS access key ID"
-}
-
-variable "aws_secret_access_key" {
-  type        = string
-  description = "AWS secret access key"
-}
-
-variable "aws_region" {
-  type        = string
-  description = "AWS region"
+  region     = "us-east-1"
 }
 
 resource "aws_ecr_repository" "ecr_repo" {
