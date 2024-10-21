f956ff52edacccc5e2eb05e51139b266da696069
Pablo <75391203+pablocappetta@users.noreply.github.com>
2024-10-21 19:18:47 -0300

    Agregada profile page

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,17 +1,15 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
+import ProfilePage from '@/components/profile/profile-page';
 
-const breadcrumbItems = [
-  { title: 'Panel', link: '/dashboard' },
-  { title: 'Perfil', link: '/profile' }
-];
+const breadcrumbItems = [{ title: 'Perfil', link: '/profile' }];
 
 export default function page() {
   return (
     <PageContainer>
       <div className="space-y-2">
         <Breadcrumbs items={breadcrumbItems} />
-        <div className={'text-2xl'}>Under construction...</div>
+        <ProfilePage />
       </div>
     </PageContainer>
   );
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,17 +1,15 @@
 import { Breadcrumbs } from '@/components/breadcrumbs';
 import PageContainer from '@/components/layout/page-container';
+import ProfilePage from '@/components/profile/profile-page';
 
-const breadcrumbItems = [
-  { title: 'Panel', link: '/dashboard' },
-  { title: 'Perfil', link: '/profile' }
-];
+const breadcrumbItems = [{ title: 'Perfil', link: '/profile' }];
 
 export default function page() {
   return (
     <PageContainer scrollable={true}>
       <div className="space-y-4">
         <Breadcrumbs items={breadcrumbItems} />
-        <div>Under construction...</div>
+        <ProfilePage />
       </div>
     </PageContainer>
   );
