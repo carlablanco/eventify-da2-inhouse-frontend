09b5e75490beb52cf5b1e07c7cda830382a68b3e
santibruno <santib1502@gmail.com>
2024-11-09 19:34:21 -0300

    preventdefault a formulario

--- api/api.ts
+++ api/api.ts
@@ -15,16 +15,13 @@ export interface LoginRequest {
 export async function loginUser(
   credentials: LoginRequest
 ): Promise<LoginResponse> {
-  const response = await fetch(
-    'http://backclusterinhouseloadbalancer-1956526135.us-east-1.elb.amazonaws.com:3001/api/v1/login',
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(credentials)
-    }
-  );
+  const response = await fetch('http://backend.eventify:3001/api/v1/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(credentials)
+  });
 
   const data = await response.json();
 
@@ -37,14 +34,12 @@ export async function loginUser(
 
 //TBC
 export async function getUser(id: string) {
-  return await fetch(
-    `http://backclusterinhouseloadbalancer-1956526135.us-east-1.elb.amazonaws.com:3001/api/v1/users/${id}`
-  );
+  return await fetch(`http://backend.eventify:3001/api/v1/users/${id}`);
 }
 
 export async function getUsers(page?: number) {
   const response = await fetch(
-    `http://backclusterinhouseloadbalancer-1956526135.us-east-1.elb.amazonaws.com:3001/api/v1/users?page=1`
+    `http://backend.eventify:3001/api/v1/users?page=1`
   ); //${(page)?page:1}
   const data = await response.json();
 
